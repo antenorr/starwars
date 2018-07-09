f91ccb3ed7f786fa6da7348dfdbabb3f822aba3d
antenorr <roudyantenor@gmail.com>
2018-07-09 18:17:57 -0400

    Play button added with animations to it an hover effect and fadout of button completed

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+
+
+    
+
+    let glow = $('.glow_effect');
+    setInterval(function () {
+        glow.toggleClass('glow');
+    }, 1000);
+
+    let play_button = $(".play_button");
+    play_button.on('click', () => {
+        play_button.fadeOut(500);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+    // Thsi is the formula to cause an element of absolute to move to the left
+
+    // $("button").click(function () {
+    //     $(".fighter").animate({ left: '250px' });
+    // });
+    /**
+     *
+     *         <button>hi</button>
+        <div class="fighter" style="background:#98bf21;height:100px;width:100px;position:absolute;">
+        </div>
+     */
