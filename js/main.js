a9916b3479feb05c0e14a3049c6d924c75db3b49
antenorr <roudyantenor@gmail.com>
2018-07-10 01:19:37 -0400

    both fighters chosen and present on stage for fight - Modal has been fadedout - now ready for fight

--- js/main.js
+++ js/main.js
@@ -10,19 +10,75 @@ $(document).ready(function () {
     let play_button = $(".play_button");
     play_button.on('click', () => {
         play_button.fadeOut(500);
-        $(".fade_in_effect").fadeIn("slow")
+        $(".fade_in_effect").fadeIn("slow");
+        $(".choose_prompt").text('Choose your 1st fighter!');
     })
 
 
 
+    // **************** BEGIN TO PROGRAM GAMER ******************
 
-    $(".choose_prompt").text('Choose your 1st fighter!')
+    const aayla = {
+        name: "aayla",
+        attack: 30,
+        img: "./../assets/aayla-secura.png"
+    }
+    const luke = {
+        name: "luke",
+        attack: 25,
+        img: "./../assets/luke-skywalker.png"
+    }
+    const boba = {
+        name: "boba",
+        attack: 20,
+        img: "./../assets/boba-fett.png"
+    }
+    const darth = {
+        name: "darth",
+        attack: 50,
+        img: "./../assets/vader.png"
+    }
 
-
-
-
-
+    let chooseFighterFlag = true;
     let selectedFighters = [];
+    let playerOne;
+    let playerTwo;
+
+    $(".fighter").on('click', function(e) {
+        if (chooseFighterFlag && selectedFighters.length < 1 ) {
+ 
+            let selection =  e.currentTarget.classList[0];
+            selectedFighters.push(eval(selection)); //eval() used to change string to variable
+            playerOne = eval(selection)
+            $(".fighter_one").html(` <img src="${playerOne.img}" alt="" class="fighting_stance">`);
+            chooseFighterFlag = false;
+            $(".choose_prompt").text('Choose your 2nd fighter!');
+          
+        }
+        else if (!chooseFighterFlag && selectedFighters.length === 1) {
+
+            let selection = e.currentTarget.classList[0];
+            selectedFighters.push(eval(selection)); //eval() used to change string to variable
+            playerTwo = eval(selection)
+            $(".fighter_two").html(` <img src="${playerTwo.img}" alt="" class="fighting_stance">`);
+            $(".choose_prompt").text('Get Ready to Rumble!');
+            $(".fade_out_effect").fadeOut(2000)
+
+        
+ 
+          
+        }
+        console.log( playerOne, playerTwo);
+    })
+   
+
+
+
+
+
+
+
+   
     const chooseFighter = () => {
 
     }
